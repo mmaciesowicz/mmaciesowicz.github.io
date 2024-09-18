828541b6737156948819f379fc1364f0e7ae772e
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-09-17 22:22:25 -0400

    added load more functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const recentProjects = document.getElementById('recent-projects');
 const arrayOfProjects = recentProjects.children;
+const loadMoreBtn = document.getElementById('loadMoreBtn');
 
 console.log(recentProjects);
 console.log(arrayOfProjects);
@@ -7,4 +8,16 @@ console.log(arrayOfProjects);
 // hide all projects except for the first one (most recent)
 for (let i=1; i<arrayOfProjects.length; i++) {
     arrayOfProjects[i].style.display = "none";
+}
+
+function loadMore() {
+    for (let i=1; i<arrayOfProjects.length; i++) {
+        if (arrayOfProjects[i].style.display === "none") {
+            arrayOfProjects[i].style.display = "block";
+        }
+        else {
+            arrayOfProjects[i].style.display = "none";
+        }
+    }
+    loadMoreBtn.innerText = loadMoreBtn.innerText === "Load More" ? "Load Less" : "Load More";
 }
